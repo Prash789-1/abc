e1641d4517f1b7f0838bf3479e644b527525fda9
Prashant <pd7157232@gmail.com>
2023-01-05 11:48:25 +0530

    test

---
+++ index.js
@@ -0,0 +1,13 @@
+const getAllUsers = (id) => monads
+
+const getuser = (id) => {
+    try {
+        let result = getuser.findAll(user);
+        console.log(result)
+        result.status(200).send(result);
+    } catch (err) {
+        console.error(err);
+    }
+}
+this file is created by parshant
+
